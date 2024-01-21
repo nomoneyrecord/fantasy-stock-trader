943634c4904d6d95ed35ad539a81a592410796ea
Gary H <3rdeye.ghj@gmail.com>
2024-01-21 14:49:53 -0800

    Utilizing PropTypes for reusable components with the InputField completed

---
+++ src/components/InputField.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InputField = ({ type, name, value, onChange, placeholder }) => {
+  return (
+    <input
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className="form-control"
+    />
+  );
+};
+
+InputField.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string
+};
+
+export default InputField; 
