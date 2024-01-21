38889d24aca1098723a92d6999fc67805e517250
Gary H <3rdeye.ghj@gmail.com>
2024-01-21 15:36:21 -0800

    Modal component completed using Proptypes for reusability

---
+++ src/components/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({ children, show, onClose }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className='modal' onClick={onClose}>
+      <div className='modal-content' onClick={e => e.stopPropagation()}>
+        <div className='modal-header'>
+          <button onClick={onClose}>&times;</button>
+        </div>
+        <div className='modal-body'>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Modal.proptypes = {
+  children: PropTypes.node,
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired
+};
+
+export default Modal;
