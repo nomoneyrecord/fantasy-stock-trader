81884f0e1892ef66198604de07e0332810086a44
Gary H <3rdeye.ghj@gmail.com>
2024-01-22 13:27:46 -0800

    SQLAlchemy configured

--- server/App.py
+++ server/App.py
@@ -1,8 +1,13 @@
 from flask import Flask;
 from flask_cors import CORS; 
+from flask_sqlalchemy import SQLAlchemy;
+
+
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLAlCHEMY.DATABASE.URI'] = 'postgresql://username:password@localhost:5432/fantast_stock_trading'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
