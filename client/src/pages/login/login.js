614ebac0f7cc40cee5d13d1712478d88ec3980ff
Gary H <3rdeye.ghj@gmail.com>
2024-01-31 12:19:18 -0800

    Implemented input fields components for the Modal and verifying Sign Up feature

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,67 +1,92 @@
-import React from 'react';
+import React from "react";
 import Button from "../../components/Button";
 import InputField from "../../components/InputField";
 import Modal from "../../components/Modal";
-import { useState } from 'react';
-
-
-
+import { useState } from "react";
 
 const Login = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-    console.log('Email: ', event.target.value);
-  }
+    console.log("Email: ", event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    console.log('Password: ', event.target.value);
-  }
+    console.log("Password: ", event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Form Submitted. Email: ", email, " Password: ", password);
+  };
+
+  const handleSignUpEmailChange = (event) => {
+    setSignUpEmail(event.target.value);
+    console.log("Sign Up Email: ", event.target.value);
+  };
+
+  const handleSignUpPasswordChange = (event) => {
+    setSignUpPassword(event.target.value);
+    console.log("Sign Up Password: ", event.target.value);
+  };
+
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault(); 
+    console.log("Sign Up Form Submitted. SignUpEmail: ", signUpEmail, " SignUpPassword ", signUpPassword);
   }
 
   const handleModalOpen = () => {
-    setModalOpen(true); 
+    setModalOpen(true);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <InputField 
-          type='text'
-          name='email'
+        <InputField
+          type="text"
+          name="email"
           value={email}
           onChange={handleEmailChange}
-          placeholder='Enter your email'
+          placeholder="Email"
           required={true}
         />
-        <InputField 
-          type='password'
-          name='password'
-          value={password}
+        <InputField
+          type="password"
+          name="Sign Up Password"
+          value={signUpPassword}
           onChange={handlePasswordChange}
-          placeholder='Enter your password'
-          required={true}  
+          placeholder="Password"
+          required={true}
         />
-        <Button 
-          text='Login'
-          onClick={() => {}}
-        />
-        <Button
-          text='Sign Up'
-          onClick={handleModalOpen}  
-          type='button'
-        />
+        <Button text="Login" onClick={() => {}} />
+        <Button text="Sign Up" onClick={handleModalOpen} type="button" />
       </form>
-      <Modal onClose={handleModalOpen}/>
+        <Modal show={modalOpen} onClose={handleModalOpen}>
+          <form onSubmit={handleSignUpSubmit}>
+          <InputField
+          type="text"
+          name="Sign Up Email"
+          value={signUpEmail}
+          onChange={handleSignUpEmailChange}
+          placeholder="Email"
+          required={true}
+        />
+        <InputField
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleSignUpPasswordChange}
+          placeholder="Password"
+          required={true}
+        />
+          </form>
+        </Modal>
     </div>
   );
 };
