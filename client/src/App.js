42297a07d24df042ac7dd26deda95a9c52af04dd
Gary H <3rdeye.ghj@gmail.com>
2024-02-06 12:26:44 -0800

    Created database with seperate create_db.py file and verified table is loading as intended in PGAdmin

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,6 @@ import React, { useEffect } from 'react';
 
 function App() {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/test');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <>
