f5198d7236dcd50d9dd695e945bf008e202fc4cc
Gary H <3rdeye.ghj@gmail.com>
2024-03-25 16:44:43 -0700

    Footer styled and adding hover effects to all buttons

--- client/src/pages/authpage/authpage.js
+++ client/src/pages/authpage/authpage.js
@@ -194,7 +194,7 @@ const AuthPage = ({ onLoginSuccess, sessionExpired }) => {
               placeholder="Password"
               required={true}
             />
-            <Button text="Submit" type="submit" className="submit-button"/>
+            <Button text="Submit" type="submit" className="submit-button" />
             {successMessage && (
               <div className="success-message">{successMessage}</div>
             )}
@@ -204,7 +204,32 @@ const AuthPage = ({ onLoginSuccess, sessionExpired }) => {
 
       {errorMessage && <div className="error-message">{errorMessage}</div>}
       <footer className="footer">
-        
+        <div className="footer-content">
+          Created By: Gary Hughes Jr
+          <div>
+            <button className="footer-button"
+              onClick={() =>
+                (window.location.href = "mailto:3rdeye.ghj@gmail.com")
+              }
+            >
+              Contact
+            </button>
+          </div>
+          <div>
+            <button className="footer-button"
+              onClick={() => window.open("https://garyhughesjr.netlify.app/")}
+            >
+              My Website
+            </button>
+          </div>
+          <div>
+            <button className="footer-button"
+              onClick={() => window.open("https://github.com/nomoneyrecord")}
+            >
+              GitHub
+            </button>
+          </div>
+        </div>
       </footer>
     </div>
   );
