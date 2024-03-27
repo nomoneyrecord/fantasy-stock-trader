3e8319618cfb8f86284da4d3bf7fdfc3bfefed4b
Gary H <3rdeye.ghj@gmail.com>
2024-03-27 11:43:00 -0700

    General styling for navbar, but still needs refinement

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// Import your logo image
+import logoImage from '../Images/logo3.png'; 
 
 const Navbar = ({ onLogout }) => {
   return (
     <nav className='navbar'>
-      <NavLink to='/home'>Home</NavLink>
-      <NavLink to='/trade'>Trade</NavLink>
-      <button onClick={onLogout}>Logout</button>
+      <div className="logo">
+        <img src={logoImage} alt="Logo" />
+        <span>StockLab</span>
+      </div>
+      <div>
+        <NavLink to='/home' activeClassName="active">Home</NavLink>
+        <NavLink to='/trade' activeClassName="active">Trade</NavLink>
+        <button onClick={onLogout}>Logout</button>
+      </div>
     </nav>
   );
 };
-
 
 export default Navbar;
