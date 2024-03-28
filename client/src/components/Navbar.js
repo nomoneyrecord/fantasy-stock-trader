b91a57b9f921c36bc89c54c918a9120f4a0431ed
Gary H <3rdeye.ghj@gmail.com>
2024-03-28 11:41:45 -0700

    Additional styling for navbar and moving back to view height bug with unwanted scroll bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 // Import your logo image
-import logoImage from '../Images/logo3.png'; 
+import logoImage from "../Images/logo3.png";
 
 const Navbar = ({ onLogout }) => {
   return (
-    <nav className='navbar'>
+    <nav className="navbar">
       <div className="logo">
         <img src={logoImage} alt="Logo" />
         <span className="navbar-appname">StockLab</span>
       </div>
       <div>
-        <NavLink to='/home' activeClassName="active">Home</NavLink>
-        <NavLink to='/trade' activeClassName="active">Trade</NavLink>
-        <button onClick={onLogout}>Logout</button>
+        <NavLink to="/home" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/trade" activeClassName="active">
+          Trade
+        </NavLink>
+        <button className="logout-button" onClick={onLogout}>
+          Logout
+        </button>
       </div>
     </nav>
   );
